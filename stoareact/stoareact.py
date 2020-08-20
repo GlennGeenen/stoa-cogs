d04e8df946fa0d06b1a2247cdae24a5f1d4da6c3
Glenn Geenen <glenn@geenentijd.be>
2020-08-20 20:28:41 +0200

    test

--- stoareact/stoareact.py
+++ stoareact/stoareact.py
@@ -104,8 +104,7 @@ class StoaReact(commands.Cog):
             return
         sentence = message.content.lower()
         for response in reacts:
-            log.info("{}".format(reacts[response]))
-            if reacts[response].lower() in sentence:
+            if set(w.lower() for w in reacts[response]).intersection(sentence):
                 try:
                     await message.channel.send(response)
                 except (discord.errors.Forbidden, discord.errors.InvalidArgument, discord.errors.NotFound):
